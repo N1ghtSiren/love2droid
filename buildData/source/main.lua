d688b9f4778e8f89994bb812f40364c203ea071e
N1ghtSiren <viviflad@gmail.com>
2020-09-30 22:47:47 +0500

    touch_testing
    3

--- buildData/source/main.lua
+++ buildData/source/main.lua
@@ -1,42 +1,20 @@
---globals
-w = love.graphics.getWidth()
-h = love.graphics.getHeight()
-circle = love.graphics.circle
+local circle = love.graphics.circle
+local touches = {}
 
-font_size_small = math.floor((love.graphics.getHeight()/20)+1)
-font_small = love.graphics.newFont("fonts/comicsans.ttf", font_size_small)
-
-font_size_big = math.floor((love.graphics.getHeight()/10)+1)
-font_big = love.graphics.newFont("fonts/umeboshi.ttf", font_size_big)
-
-font_size_main = math.floor((love.graphics.getHeight()/15)+1)
-font_main = love.graphics.newFont("fonts/umeboshi.ttf", font_size_main)
-
-rgba = {}
-rgba[1] = {1, 1, 1, 0.7} 
-rgba[2] = {1, 0.28, 0.10, 0.7}
-rgba[3] = {0, 0.6, 0, 0.7}
-rgba[4] = {0, 0.36, 90, 0.7}
-
-brgba = {}
-brgba[1] = {0, 0, 0, 1}
-brgba[2] = {0, 0, 0, 1}
-brgba[3] = {0, 0, 0, 1}
-brgba[4] = {0, 0, 0, 1}
---endglobals
-
-
-require("libs/touches")
-require("libs/addon")
-require("libs/TRASH")
-
-local function f()
-    local t = touches.get()
-
-    for k,_ in pairs(t) do
-        circle("fill",t[k][1],t[k][2],30)
+function love.update(dt)
+    --touches
+    for i, id in ipairs(love.touch.getTouches()) do
+        table.insert(touches,{love.touch.getPosition(id)})
     end
-
-    touches.clear()
+    --mouse
+    if(love.mouse.isDown(1))then
+		table.insert(touches,{love.mouse.getPosition()})
+	end
 end
-addon.drawAdd(f,1)
+
+function love.draw()
+    for k,v in pairs(touches) do
+        circle("fill",v[1], v[2], 30)
+        table.remove(touches,k)
+    end
+end
